5becab7eaebaa5e53d60b52c8209a01e9e8ecc9e
Liza <lisalisalisa57@mail.ru>
2024-07-27 10:55:08 +0300

    folder tests moved

--- src/js/__tests__/test-critical.js
+++
@@ -1,9 +0,0 @@
-import { user, defineHealth } from "../src/js/app.js";
-describe("defineHealth", () => {
-  it("should log 'critical' if health is less than or equal to 15", () => {
-    const mockUser = { ...user, health: 10 };
-    console.log = jest.fn();
-    defineHealth(mockUser);
-    expect(console.log).toHaveBeenCalledWith("critical");
-  });
-});
--- src/js/__tests__/test-healthy.js
+++
@@ -1,9 +0,0 @@
-import { user, defineHealth } from "../src/js/app.js";
-describe("defineHealth", () => {
-  it("should log 'healthy' if health is greater than 50", () => {
-    const mockUser = { ...user, health: 60 };
-    console.log = jest.fn();
-    defineHealth(mockUser);
-    expect(console.log).toHaveBeenCalledWith("healthy");
-  });
-});
